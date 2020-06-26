6be08fdc1510553282b0920b5480e07ef5ad3783
dudarvicky <dudarvicky>
2020-06-26 12:11:15 -0500

    Roman : Day 7 (Final change)

--- JavaProjectAM/src/Day7/Final_Method.java
+++ JavaProjectAM/src/Day7/Final_Method.java
@@ -1,26 +1,19 @@
-package Day7;
-
-	final class Bike       // final class, so we cannot extend into child class
-	{
-		final void run()   // final method, so we cannot override this method
-		{
-			System.out.println("Running....");
-		}
-	}
-
-    class Honda extends Bike   // compile time error
-    {
-    	void run()   // compile time error
-		{
-			System.out.println("Started....");
-		}
-    }
-
-public class Final_Method {
-
-	public static void main(String[] args) {
-	
-
-	}
-
-}
+/*
+ * package Day7;
+ * 
+ * // final class Bike // final class, so we cannot extend into child class {
+ * final void run() // final method, so we cannot override this method {
+ * System.out.println("Running...."); } }
+ * 
+ * // class Honda extends Bike // compile time error { void run() // compile
+ * time error { System.out.println("Started...."); } }
+ * 
+ * public class Final_Method {
+ * 
+ * public static void main(String[] args) {
+ * 
+ * 
+ * }
+ * 
+ * }
+ */
--- JavaProjectAM/src/Day7/Final_Variable.java
+++ JavaProjectAM/src/Day7/Final_Variable.java
@@ -9,9 +9,9 @@ public class Final_Variable {
 	
 	public static void main(String[] args) {
 		
-		Final_Variable fm=new Final_Variable();
-		fm.speed=100;   /compile time eroor
-		System.out.println(fm.speed);
+//		Final_Variable fm=new Final_Variable();
+//		fm.speed=100;   /compile time eroor
+//		System.out.println(fm.speed);
 	}
 
 }
